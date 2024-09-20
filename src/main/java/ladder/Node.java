8fa2890cc0e2b97277693e3953b9dca61698b168
hamhyeongju <gudwn357@gmail.com>
2024-09-20 18:07:59 +0900

    refactor : add Node class for wrap int array value

---
+++ src/main/java/ladder/Node.java
@@ -0,0 +1,51 @@
+package ladder;
+
+import static ladder.Direction.*;
+
+public class Node {
+
+    private Direction direction;
+
+    private Node(Direction direction) {
+        this.direction = direction;
+    }
+
+    public static Node from(Direction direction) {
+        return new Node(direction);
+    }
+
+    public void move(Position position) {
+        if (isRight()) {
+            position.next();
+            return;
+        }
+        if (isLeft()) {
+            position.prev();
+            return;
+        }
+    }
+
+    public void setRightNode() {
+        direction = RIGHT;
+    }
+
+    public void setLeftNode() {
+        direction = LEFT;
+    }
+
+    public boolean isAlreadySetDirection() {
+        return isNone();
+    }
+
+    private boolean isRight() {
+        return direction == RIGHT;
+    }
+
+    private boolean isLeft() {
+        return direction == LEFT;
+    }
+
+    private boolean isNone() {
+        return direction == NONE;
+    }
+}
